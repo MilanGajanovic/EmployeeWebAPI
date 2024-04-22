fa6a75f24d5270b95d3d220946431c6c5f41b508
Milan_Gajanovic <milan.gajanovic@nites.eu>
2024-04-22 14:40:47 +0200

    WEB API Client and push to database

---
+++ MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using MVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployeeModel> empList = null;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                empList = JsonConvert.DeserializeObject<IEnumerable<mvcEmployeeModel>>(content);
+            }
+            else
+            {
+                // Handle unsuccessful response
+                ModelState.AddModelError(string.Empty, "Failed to retrieve employee data: " + response.ReasonPhrase);
+            }
+
+            return View(empList);
+
+        }
+    }
+}
